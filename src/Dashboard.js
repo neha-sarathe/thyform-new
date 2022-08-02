fab35d7391fa5b6f06af34c751cc2e47614c444b
neha-sarathe <neha.sarathe@engineermaster.in>
2022-08-02 19:44:34 +0530

    dashboard new

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,7 +11,7 @@ export const Dashboard = (props) => {
             <div class="col-sm-12 dashboard-margin">
               <div class="home-tab">
                 <div class="d-sm-flex align-items-center justify-content-between border-bottom">
-                  <ul class="nav nav-tabs" role="tablist">
+                  {/* <ul class="nav nav-tabs" role="tablist">
                     <li class="nav-item">
                       <a class="nav-link active ps-0" id="home-tab" data-bs-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
                     </li>
@@ -24,13 +24,13 @@ export const Dashboard = (props) => {
                     <li class="nav-item">
                       <a class="nav-link border-0" id="more-tab" data-bs-toggle="tab" href="#more" role="tab" aria-selected="false">More</a>
                     </li>
-                  </ul>
+                  </ul> */}
                   <div>
-                    <div class="btn-wrapper">
+                    {/* <div class="btn-wrapper">
                       <a href="#" class="btn btn-otline-dark align-items-center"><i class="icon-share"></i> Share</a>
                       <a href="#" class="btn btn-otline-dark"><i class="icon-printer"></i> Print</a>
                       <a href="#" class="btn btn-primary text-white me-0"><i class="icon-download"></i> Export</a>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
                 <div class="tab-content tab-content-basic">
