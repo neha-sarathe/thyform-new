8a99cc006ad95cf10ca1dde3fa9cde6a1df61d34
hemlatarajpoot6 <hemlata.rajpoot@engineermaster.in>
2022-09-16 15:18:56 +0530

    adding validations

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -14,6 +14,68 @@ export const MyProfile = (props) => {
   };
 
   console.log('props', props);
+
+  const [name, setName] = useState('');
+  const onName = e => {
+    const { value } = e.target;
+
+    console.log('Input value: ', value);
+
+    const re = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/;
+    if (value === "" || re.test(value)) {
+      setName(value);
+    }
+  }
+
+  // const [email, setEmail] = useState('');
+  // const onEmail = e => {
+  //   const { value } = e.target;
+
+  //   console.log('Email value: ', value);
+
+  //   const rex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{1,}$/;
+  //   if (value === "" || rex.test(value)) {
+  //     setEmail(value);
+  //   }
+  // }
+
+  const [mobile, setMobile] = useState('');
+  const onMobile = e => {
+    const { value } = e.target;
+
+    console.log('Input value: ', value);
+
+    const re = /^[0-9\b]+$/;
+    if (value === "" || re.test(value)) {
+      setMobile(value);
+    }
+  }
+
+  const [address, setAddress] = useState('');
+  const onAddress = e => {
+    const { value } = e.target;
+
+    console.log('Input value: ', value);
+
+    const re = /^[a-zA-Z0-9\s,'-]*$/;
+    if (value === "" || re.test(value)) {
+      setAddress(value);
+    }
+  }
+
+  const [companyName, setCompanyName] = useState('');
+  const oncompanyName = e => {
+    const { value } = e.target;
+
+    console.log('Input value: ', value);
+
+    const re = /^[a-zA-Z ]*$/;;
+    if (value === "" || re.test(value)) {
+      setCompanyName(value);
+    }
+  }
+
+
   return (
     <>
 
@@ -37,59 +99,59 @@ export const MyProfile = (props) => {
               <div className='mt-5'>
                 <label htmlFor="upload-button">
                   <i className="fa fa-pencil-square fa-stack-2x ml-0 icon_width" aria-hidden="true" >
-                <input
-                  type="file"
-                  id="upload-button"
-                  style={{ display: "none" }}
-                  onChange={handleChange1}
-                /></i></label>
+                    <input
+                      type="file"
+                      id="upload-button"
+                      style={{ display: "none" }}
+                      onChange={handleChange1}
+                    /></i></label>
               </div>
             </div>
             <form>
               <div class="row mt-4">
-                <div class="search-input-div col-md-12">
+                <div class="search-input-div col-md-6">
                   <label>Name</label>
-                  <input type="text" className={"search-input "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} placeholder="Name" />
+                  <input type="text" className={"search-input " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} placeholder="Name" value={name} onChange={onName} />
                 </div>
-                <div class="search-input-div col-md-4">
-                  
-                  </div>
+                <div class="search-input-div col-md-6">
+
+                </div>
                 <div class="search-input-div col-md-6 pt-4">
-                  <label for="inputEmail4">Email</label>
-                  <input type="email" class={"search-input "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="inputEmail4" placeholder="Email" />
+                  <label>Email</label>
+                  <input type="text" className={"search-input " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} placeholder="Email" />
                 </div>
-                <div class="form-group col-md-6">
-                  
-                  </div>
+                <div class="search-input-div col-md-6">
+
+                </div>
               </div>
               <div className='row'>
                 <div class="search-input-div col-md-6 pt-4">
                   <label for="inputAddress">Mobile Number</label>
-                  <input type="tel" class={"search-input "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="number" placeholder="Mobile Number" />
+                  <input type="tel" class={"search-input " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="number" placeholder="Mobile Number" value={mobile} onChange={onMobile} />
                 </div>
-                <div class="form-group col-md-6">
-                  
-                  </div>
+                <div class="search-input-div col-md-6">
+
+                </div>
                 <div class="search-input-div col-md-6 pt-4">
                   <label for="inputAddress">Address</label>
-                  <input type="text" class={"search-input "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="inputAddress" placeholder="Address" />
+                  <input type="text" class={"search-input " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="inputAddress" placeholder="Address" value={address} onChange={onAddress} />
                 </div>
               </div>
               <div class="row">
                 <div class="search-input-div col-md-6 pt-4">
                   <label>Company Name</label>
-                  <input type="text" class={"search-input "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} placeholder="Company Name" />
+                  <input type="text" class={"search-input " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} placeholder="Company Name" value={companyName} onChange={oncompanyName} />
                 </div>
-                <div class="form-group col-md-6">
-                  
-                  </div>
+                <div class="search-input-div col-md-6">
+
+                </div>
                 <div class="search-input-div col-md-6 pt-4">
                   <label for="exampleFormControlTextarea2">Company Address</label>
-                  <textarea class={"search-input rounded-3 "+(props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="exampleFormControlTextarea2" rows="3"></textarea>
+                  <textarea class={"search-input rounded-3 " + (props.darkmodes ? " bg-dark login-dark-text " : " bg-light login-light-text ")} id="exampleFormControlTextarea2" rows="3"></textarea>
                 </div>
               </div>
               <div className='pt-4'>
-              <button type="submit" class="btn btn-primary">Save Change</button>
+                <button type="submit" class="btn btn-primary">Save Change</button>
               </div>
             </form>
           </div>
